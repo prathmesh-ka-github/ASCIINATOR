32e86ee40ca40f86d5d4f7ede06acbe22f1fe313
prathmesh-ka-github <pkale@albany.edu>
2025-08-05 18:53:12 -0400

    :hammer: integrated react router dom

---
+++ client/src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div>
+        <NavLink to="/">Home</NavLink>
+        Main
+    </div>
+  )
+}
+
+export default Main
