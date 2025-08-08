15876b618dded4aaa9c076029f420a66f45ac729
sri-nivas1227 <srinivasmekala1227@gmail.com>
2025-08-07 23:43:18 -0400

    added fonts, installed Pillow

--- server/asciinatorEndpoint.py
+++ server/asciinatorEndpoint.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request
 from Services.AsciinatorService import convert_to_ascii_image 
 from flask import send_file
-
+from PIL import Image
+import io
 asciinatorRouter = Blueprint('asciinator', __name__)
 
 """
@@ -11,22 +12,22 @@ Takes an image file from the request, processes it, and returns an ASCII art rep
 def asciinator():
     if 'file' not in request.files:
         return {'error': 'No file part in the request'}, 400
-    
+    print("fetching file")
     file = request.files['file']
-    
+    print(file.filename)
     if file.filename == '':
         return {'error': 'No selected file'}, 400
-    
-    try:
-        image = file.read()
-        input_file_name = file.filename
-        output_file_name = "output-"+request.form.get('output_file_name', input_file_name)
-        font_size = int(request.form.get('font_size', 30))      
-        # print(image) 
-        return send_file(image, mimetype='image/jpg', as_attachment=True, download_name=output_file_name)
-    except Exception as e:  
-        return {'error': str(e)}, 500
+    # read the image file in binary mode
+    image = Image.open(file)
+    # convert the image to ASCII art
+    ascii_image = convert_to_ascii_image(image)
+    ## save image to buffer
+    img_io = io.BytesIO()
+    ascii_image.save(img_io, format='PNG')
+    img_io.seek(0)
 
-        
+    return send_file(img_io, mimetype='image/png')
+
+    
 
     
